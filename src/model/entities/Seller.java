92f32c268c81a02d10ef4186d6467b4c72844773
Raphael <raphael.feijo@edu.unifil.br>
2025-10-31 17:30:11 -0300

    Seller entity class

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,103 @@
+package model.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birtDate;
+    private Double baseSalary;
+
+    private Department department;
+
+    public Seller(){}
+
+    public Seller(Integer id, String name, String email, Date birtDate, Double baseSalary, Department department) {
+        this.baseSalary = baseSalary;
+        this.birtDate = birtDate;
+        this.email = email;
+        this.name = name;
+        this.id = id;
+        this.department = department;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Date getBirtDate() {
+        return birtDate;
+    }
+
+    public void setBirtDate(Date birtDate) {
+        this.birtDate = birtDate;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Seller seller = (Seller) o;
+        return Objects.equals(id, seller.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Seller{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birtDate=" + birtDate +
+                ", baseSalary=" + baseSalary +
+                ", department=" + department +
+                '}';
+    }
+}
